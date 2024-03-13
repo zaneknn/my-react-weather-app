591fe32a0b954aedbcf5d841e6ff934f52f00e27
Zane Jakuseva <zane.knn@gmail.com>
2024-03-13 13:58:28 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,22 +3,29 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather app</h1>
-      <footer>
-        This project was coded by{" "}
-        <a href="https://github.com/zaneknn" target="_blank" rel="noreferrer" s>
-          Zane Jakuseva
-        </a>{" "}
-        and is open-sourced on{" "}
-        <a
-          href="https://github.com/zaneknn/my-react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>
-        .
-      </footer>
+      <div className="container">
+        <h1>Weather app</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/zaneknn"
+            target="_blank"
+            rel="noreferrer"
+            s
+          >
+            Zane Jakuseva
+          </a>{" "}
+          and is open-sourced on{" "}
+          <a
+            href="https://github.com/zaneknn/my-react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+          .
+        </footer>
+      </div>
     </div>
   );
 }
